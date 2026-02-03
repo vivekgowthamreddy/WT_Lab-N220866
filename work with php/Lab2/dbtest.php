ebaa0459e5c9e17e7268abb4624405b122ccd6b7
vivekgowthamreddy <n220866@rguktn.ac.in>
2026-02-03 16:53:50 +0530

    Revert "Add project files under 'work with php'"
    This reverts commit 427d1b22078f1f5034c305519edbc612271df324.

--- work with php/Lab2/dbtest.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "testdb");
-
-if (!$conn) {
-    die("Database connection failed");
-}
-?>
--- work with php/dbtest.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "testdb");
-
-
-if (!$conn) {
-    die("Database connection failed");
-}
-
-
